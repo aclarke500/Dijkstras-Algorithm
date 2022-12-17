23b40334cb55c49a1350bb6ff7874c5a4cdbfe0f
Vangekillsvado <aclarke500@gmail.com>
2022-12-16 19:08:31 -0500

    Tidied up code, added comments

--- dijkstras_algorithim.py
+++ dijkstras_algorithim.py
@@ -1,6 +1,5 @@
 
 
-
 def get_index_of_vertex(V, v):
     for i in range(len(V)):
         if v == V[i]:
@@ -8,80 +7,68 @@ def get_index_of_vertex(V, v):
 
     return None
 
+
 def done(distances):
     for d in distances:
         if d == float('inf'):
             return False
 
-
-
-
-
-
+'''
+    Implements Dijkstra's algorithim
+'''
 def dijkstra(V, E, source):
+
     unvisted = V.copy()
-    visted = []
-
     shortest_distance = []
-    previous_vertex = [None, None, None, None, None]
-
     vi_neighbours = []
 
     # get neighbours
     for v in V:
+        # neighbours of particular node
         neighbours = []
         for e in E:
-
+            # if v is part of an edge, add the vertex is not v
             if e[0] == v:
                 neighbours.append(e[1])
             elif e[1] == v:
                 neighbours.append(e[0])
-
+        # adds list of neighbours at correct index
         vi_neighbours.append(neighbours)
 
-    for v in V:
-        if v == source:
+    for v in V: # set distance to source to 0, all other nodes could 
+        if v == source: # be up to infinity
             shortest_distance.append(0)
-        else:
+        else: 
             shortest_distance.append(float('inf'))
 
 
-    # print(shortest_distance)
-    for i in range(0, 5):
+    for i in range(0, len(V)):
+        # bad practice but it works for the assignment
+        temp = 99999
+        current_node = None
+
+        for v in unvisted:
+            if shortest_distance[get_index_of_vertex(V, v)] < temp:
+                current_node = v
+ 
+        neighbours = vi_neighbours[get_index_of_vertex(V, current_node)]
+        distance = shortest_distance[get_index_of_vertex(V, current_node)]
+        for n in neighbours:
+            temp_dist = distance+1
+            neighbour_index = get_index_of_vertex(V, n)
+
+            if shortest_distance[neighbour_index] > temp_dist:
+                shortest_distance[neighbour_index] = temp_dist
 
 
-
-
-            temp = 99999
-            current_node = None
-            for v in unvisted:
-                if shortest_distance[get_index_of_vertex(V, v)] < temp:
-                    current_node = v
-                    # print(v, "ste")
-            # print(current_node)
-            # print(current_node, 'cn', V)
-            neighbours=vi_neighbours[get_index_of_vertex(V, current_node)]
-            distance=shortest_distance[get_index_of_vertex(V, current_node)]
-            for n in neighbours:
-                temp_dist=distance+1
-                neighbour_index = get_index_of_vertex(V, n)
-                # print(neighbour_index)
-                if shortest_distance[neighbour_index] > temp_dist:
-                    shortest_distance[neighbour_index] = temp_dist
-                    # previous_vertex[neighbour_index] = current_node
-
-            unvisted.remove(current_node)
-            # print(unvisted)
-        
-
-   
-
-
+        unvisted.remove(current_node)
 
 
         # vertices will be strings v1 ... vn,
     print(shortest_distance)
-        # sample list for practice
+
+
+    # sample list for practice
 V = ['v1', 'v2', 'v3', 'v4', 'v5', 'v6']
 
 
